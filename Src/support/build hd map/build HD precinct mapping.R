fe1eb787e407804e41d378309fd73ce597e4cd26
CAMP-walker <103772696+CAMP-walker@users.noreply.github.com>
2023-03-22 16:32:38 -0400

    Add supporting files

---
+++ Src/support/build hd map/build HD precinct mapping.R
@@ -0,0 +1,32 @@
+
+library(dplyr)
+
+#precinct_paths <- file.path("Assets/election", list.files("Assets/election/", pattern="Daily_Registrant_Count*"))
+
+#precinct_files <- lapply(precinct_paths, read.csv)
+
+#precinct_c
+house_precincts <- read.csv(file.path("Assets/election", list.files("Assets/election/", "Daily_Registrant_Count_By_House*")), fileEncoding="UTF-8-BOM")
+
+house_precincts$Locality <- sub("Locality: \\d{3} ", "", house_precincts$Locality)
+house_precincts$District <- as.numeric(sub("HSE ", "", house_precincts$District))
+
+corrections <- read.csv("Assets/election/Corrections_Table.csv")
+
+#for( i in 1:nrow(corrections) )  {
+#    house_precincts$PrecinctName <- sub(corrections$PrecinctName, corrections$FormerName[i], house_precincts$PrecinctName)
+#}
+
+
+results <- read.csv("Assets/election/2021_November_General_Results.csv")
+
+
+
+# df_in$key <- paste0(df_in$State_House_District,"_",df_in$Precinct)
+# df_in %>% select(key) %>% unique() %>% nrow()
+
+# mapping_table <- df_in %>%
+#  distinct(State_House_District,Precinct) %>% arrange(State_House_District,Precinct)
+
+# # mapping_table
+# write.csv(mapping_table,"Assets\\election\\VA_HD-Precinct_MappingTable.csv",row.names=FALSE)
---
+++ Src/support/finance summary data.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+distSummary <- data.frame(dist = seq(1,5),test_val = c("a","b","c","d","e"))
+
+campFin <- data.frame(
+  dist = c(1,1,2,2,3,3,4,4,5,5),
+  party = c("R","D","R","D","R","D","R","D","R","D"),
+  candidate = c("Ricky","Drake","Ryan","Dennis","Robert","Drew","Richard","Dylan","Ronald","Donald"),
+  cash = c(500,200,400,300,250,250,100,1000,50,350)
+)
+
+camFin_summary <- campFin %>%
+  group_by(dist, party) %>%
+  summarise(total_cash = sum(cash), candidate = first(candidate)) %>%
+  pivot_wider(names_from = party, values_from = c(total_cash, candidate), names_sep = "_")
+
+output <- merge(distSummary, camFin_summary, by = "dist", all = TRUE)
+output
